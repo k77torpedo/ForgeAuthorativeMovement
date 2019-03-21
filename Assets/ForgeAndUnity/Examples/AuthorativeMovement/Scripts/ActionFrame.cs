ffdd1b0fc1ccb3e60c162945387d2d9571b32d44
DEV-Segaia <segaia@gmx.de>
2019-03-21 19:20:56 +0100

    + Initial commit

---
+++ Assets/ForgeAndUnity/Examples/AuthorativeMovement/Scripts/ActionFrame.cs
@@ -0,0 +1,9 @@
+﻿/// <summary>
+/// A frame of action a player has done with a <see cref="InputListener"/>.
+/// </summary>
+[System.Serializable]
+public struct ActionFrame {
+    //Fields
+    public byte actionId;
+    public byte[] data;
+}
---
+++ Assets/ForgeAndUnity/Examples/AuthorativeMovement/Scripts/ExtensionMethods.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+/// <summary>
+/// Extension-methods for Forge Networking Remastered.
+/// </summary>
+public static class ExtensionMethods {
+    //Functions
+    public static byte[] ObjectToByteArray (this object obj) {
+        if (obj == null) {
+            return null;
+        }
+
+        BinaryFormatter bf = new BinaryFormatter();
+        MemoryStream ms = new MemoryStream();
+        bf.Serialize(ms, obj);
+
+        return ms.ToArray();
+    }
+
+    public static T ByteArrayToObject<T> (this byte[] arrBytes) {
+        MemoryStream ms = new MemoryStream();
+        BinaryFormatter bf = new BinaryFormatter();
+        ms.Write(arrBytes, 0, arrBytes.Length);
+        ms.Seek(0, SeekOrigin.Begin);
+        T obj = (T)bf.Deserialize(ms);
+
+        return obj;
+    }
+}
